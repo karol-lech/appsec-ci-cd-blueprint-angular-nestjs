c2a5242fa2f952c1cc9a2db95861f5b26d6017b0
Karol Lech <karollech787@gmail.com>
2026-01-06 19:35:41 +0100

    feat: sql injection example

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
+  constructor(private readonly dataSource: DataSource) {}
+
+  getData(id?: string): { message: string } {
+    this.dataSource.query(`SELECT id, message FROM messages WHERE id = ${id}`);
+
     return { message: 'Hello API' };
   }
 }
